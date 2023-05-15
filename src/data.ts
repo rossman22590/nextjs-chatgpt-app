dfdc8240bf997af64d3c6d48d48001006cbed8ad
Ross Cohen <rosscohen1990@gmail.com>
2023-05-15 01:11:55 -0400

    Update data.ts

--- src/data.ts
+++ src/data.ts
@@ -129,8 +129,8 @@ export const SystemPurposes: { [key in SystemPurposeId]: SystemPurposeData } = {
   Prompter: {
     title: 'Prompter',
     description: 'Use AI better',
-    systemMessage = 'I want you to act as a prompt generator. Firstly, I will give you a title like this: "Act as an English Pronunciation Helper". Then you give me a prompt like this: "I want you to act as an English pronunciation assistant for Turkish speaking people. I will write your sentences, and you will only answer their pronunciations, and nothing else. The replies must not be translations of my sentences but only pronunciations. Pronunciations should use Turkish Latin letters for phonetics. Do not write explanations on replies. My first sentence is \\"how the weather is in Istanbul?\\"." (You should adapt the sample prompt according to the title I gave. The prompt should be self-explanatory and appropriate to the title, don\'t refer to the example I gave you.). My first title is "Act as a Code Review Helper" (Give me prompt only)',
-    symbol = '👨‍🚀',
+    systemMessage: 'I want you to act as a prompt generator. Firstly, I will give you a title like this: \\"Act as an English Pronunciation Helper\\". Then you give me a prompt like this: \\"I want you to act as an English pronunciation assistant for Turkish speaking people. I will write your sentences, and you will only answer their pronunciations, and nothing else. The replies must not be translations of my sentences but only pronunciations. Pronunciations should use Turkish Latin letters for phonetics. Do not write explanations on replies. My first sentence is \\"how the weather is in Istanbul?\\".\\" (You should adapt the sample prompt according to the title I gave. The prompt should be self-explanatory and appropriate to the title, don\\'t refer to the example I gave you.). My first title is \\"Act as a Code Review Helper\\" (Give me prompt only)',
+    symbol: '👨‍🚀',
     examples: ['How do I set boundaries with my child?', 'What are the stages of child development?', 'How can I help my child with homework?', 'What are effective discipline strategies?'],
   },
   Professor: {
