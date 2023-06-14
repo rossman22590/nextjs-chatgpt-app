1fc632cc4c38230ad2248685869c328b5bd92cef
Ross Cohen <rosscohen1990@gmail.com>
2023-06-13 22:57:45 -0400

    Update SupportMenuItem.tsx

--- src/common/layouts/appbar/SupportMenuItem.tsx
+++ src/common/layouts/appbar/SupportMenuItem.tsx
@@ -15,22 +15,6 @@ function DiscordIcon(props: { sx?: SxProps }) {
       <path d="M4 13h6a1 1 0 001-1V4a1 1 0 00-1-1H4a1 1 0 00-1 1v8a1 1 0 001 1zm-1 7a1 1 0 001 1h6a1 1 0 001-1v-4a1 1 0 00-1-1H4a1 1 0 00-1 1v4zm10 0a1 1 0 001 1h6a1 1 0 001-1v-7a1 1 0 00-1-1h-6a1 1 0 00-1 1v7zm1-10h6a1 1 0 001-1V4a1 1 0 00-1-1h-6a1 1 0 00-1 1v5a1 1 0 001 1z" fill="currentColor" />
     </SvgIcon>;
 }
-function BringTheLove(props: { text: string, link: string, icon: JSX.Element }) {
-  const [loved, setLoved] = React.useState(false);
-  const icon = loved ? '❤️' : props.icon; // '❤️' : '🤍';
-  return <Button
-    color='neutral'
-    component={Link} noLinkStyle href={props.link} target='_blank'
-    onClick={() => setLoved(true)}
-    endDecorator={icon}
-    sx={{
-      background: 'transparent',
-      // '&:hover': { background: props.theme.palette.neutral.solidBg },
-      '&:hover': { animation: `${cssRainbowColorKeyframes} 5s linear infinite` },
-    }}>
-    {props.text}
-  </Button>;
-}
 
 
 function BringTheLove(props: { text: string, link: string, icon: React.JSX.Element }) {
@@ -74,7 +58,7 @@ export function SupportMenuItem() {
         {Brand.Meta.SiteName}
       </Box>
       <BringTheLove text='Discord' icon={<DiscordIcon sx={{ color: iconColor }} />} link={Brand.URIs.SupportInvite} />
-      <BringTheLove text='GitHub' icon={<GitHubIcon sx={{ color: iconColor }} />} link={Brand.URIs.OpenRepo} />
+     
     </ListItem>
   );
 }
