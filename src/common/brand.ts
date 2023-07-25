8fcb64b0f4a2c80d8b3505c75ed048fd0833e753
Ross Cohen <rosscohen1990@gmail.com>
2023-07-25 00:16:07 -0400

    Update brand.ts

--- src/common/brand.ts
+++ src/common/brand.ts
@@ -12,17 +12,17 @@ export const Brand = {
     Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
-    Description: 'Leading open-source AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Harnessing AI for You',
-    ThemeColor: '#434356',
+    Description: 'Leading AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
+    SiteName: 'AI Tutor Plus',
+    ThemeColor: '#d923b4',
     TwitterSite: '@enricoros',
   },
   URIs: {
-    Home: 'https://big-agi.com',
+    Home: 'https://myapps.ai',
     // App: 'https://get.big-agi.com',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
-    OpenRepo: 'https://github.com/enricoros/big-agi',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    OpenRepo: 'https://platform.myapps.ai/access',
+    SupportInvite: 'https://platform.myapps.ai/access',
     // Twitter: 'https://www.twitter.com/enricoros',
   },
 };
