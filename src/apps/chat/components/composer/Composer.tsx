706cb76721de9ebd9ef76281216296aa8415df46
Ross Cohen <rosscohen1990@gmail.com>
2023-11-11 13:40:02 -0500

    Update Composer.tsx

--- src/apps/chat/components/composer/Composer.tsx
+++ src/apps/chat/components/composer/Composer.tsx
@@ -119,14 +119,27 @@ const MicContinuationButton = (props: { variant: VariantProp, color: ColorPalett
   </Tooltip>;
 
 const CallButtonMobile = (props: { disabled?: boolean, onClick: () => void, sx?: SxProps }) =>
-  <IconButton variant='soft' color='primary' disabled={props.disabled} onClick={props.onClick} sx={props.sx}>
+  <IconButton variant='soft' color='primary' onClick={props.onClick} sx={props.sx}>
     <CallIcon />
   </IconButton>;
 
 const CallButtonDesktop = (props: { disabled?: boolean, onClick: () => void, sx?: SxProps }) =>
-  <Button variant='soft' color='primary' disabled={props.disabled} onClick={props.onClick} endDecorator={<CallIcon />} sx={props.sx}>
+  <Button variant='soft' color='primary'  onClick={props.onClick} endDecorator={<CallIcon />} sx={props.sx}>
     Call
   </Button>;
+
+// const CallButtonDesktop = (props: { disabled?: boolean, onClick: () => void, sx?: SxProps }) =>
+//   <Button variant='soft' color='primary' disabled={props.disabled} onClick={props.onClick} endDecorator={<CallIcon />} sx={props.sx}>
+//     Call
+//   </Button>;
+
+// const CallButtonMobile = (props: { disabled?: boolean, onClick: () => void, sx?: SxProps }) =>
+//   <IconButton variant='soft' color='primary' disabled={props.disabled} onClick={props.onClick} sx={props.sx}>
+//     <CallIcon />
+//   </IconButton>;
+
+
+
 
 const DrawOptionsButtonMobile = (props: { onClick: () => void, sx?: SxProps }) =>
   <IconButton variant='soft' color='warning' onClick={props.onClick} sx={props.sx}>
