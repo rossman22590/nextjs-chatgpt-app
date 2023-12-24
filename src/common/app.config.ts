c4a368cac8e509646d29f238809c6349a9d8a42a
Ross Cohen <rosscohen1990@gmail.com>
2023-12-23 20:47:13 -0500

    Update app.config.ts

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -1,8 +1,8 @@
- */
+*/
 export const Brand = {
   Title: {
     Base: 'AI Tutor Plus',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'AI Tutor Plus',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
   },
   Meta: {
     Description: 'AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
@@ -15,8 +15,8 @@ export const Brand = {
     // App: 'https://get.big-agi.com',
     CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
     OpenRepo: 'https://github.com/enricoros/big-agi',
-    OpenProject: 'https://github.com/users/enricoros/projects/4',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+	@@ -27,4 +27,4 @@ export const Brand = {
     // Twitter: 'https://www.twitter.com/enricoros',
     PrivacyPolicy: 'https://big-agi.com/privacy',
   },
+};
