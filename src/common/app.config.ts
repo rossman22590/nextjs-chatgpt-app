8a20d9cfc6b96989cacb97160893614f59368813
Ross Cohen <rosscohen1990@gmail.com>
2023-12-23 20:44:07 -0500

    Update app.config.ts

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,22 +8,21 @@
  */
 export const Brand = {
   Title: {
-    Base: 'AI Tutor Plus',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'AI Tutor',
+    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'AI Tutor',
   },
   Meta: {
-    Description: 'AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
-    SiteName: 'AI Tutor | Unlock AI Tools',
-    ThemeColor: '#6a0083',
+    Description: 'Leading AI web interface to help you learn, think, and do. AI personas, superior privacy, advanced features, and fun UX.',
+    SiteName: 'AI Tutor Plus',
+    ThemeColor: '#d923b4',
     TwitterSite: '@tsi_org',
   },
   URIs: {
     Home: 'https://myapps.ai',
     // App: 'https://get.big-agi.com',
-    CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
-    OpenRepo: 'https://github.com/enricoros/big-agi',
-    OpenProject: 'https://github.com/users/enricoros/projects/4',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
+    CardImage: 'https://myapps.ai/images/gfx/feature/tutorai_screenshot1.png',
+    OpenRepo: 'https://account.myapps.ai/dashboard',
+    SupportInvite: 'https://account.myapps.ai/dashboard',
     // Twitter: 'https://www.twitter.com/enricoros',
     PrivacyPolicy: 'https://big-agi.com/privacy',
   },
