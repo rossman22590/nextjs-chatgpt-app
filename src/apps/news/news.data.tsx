dbbba527d10389a6c56f6e73aef5cd88b32e9b96
Ross Cohen <rosscohen1990@gmail.com>
2023-12-27 20:49:39 -0500

    Update news.data.tsx

--- src/apps/news/news.data.tsx
+++ src/apps/news/news.data.tsx
@@ -46,7 +46,7 @@ export const newsCallout =
         <Grid xs={12} sm={5} sx={{ display: 'flex', flexAlign: 'center', justifyContent: 'center' }}>
           <Button
             fullWidth variant='plain' color='primary' endDecorator={<LaunchIcon />}
-            component={Link} href={RIssues + '/new?template=roadmap-request.md&title=%5BSuggestion%5D'} noLinkStyle target='_blank'
+            component={Link} href={RIssues + '#'} noLinkStyle target='_blank'
           >
             Suggest a Feature
           </Button>
@@ -81,7 +81,7 @@ export const NewsItems: NewsItem[] = [
       { text: <>Updated Ollama (v0.1.17) and OpenRouter models</> },
       { text: <>More: fixed ⌘ shortcuts on Mac</> },
       { text: <><Link href='https://big-agi.com'>Website</Link>: official downloads</> },
-      { text: <>Easier Vercel deployment, documented <Link href='https://github.com/enricoros/big-AGI/issues/276#issuecomment-1858591483'>network troubleshooting</Link></>, dev: true },
+      { text: <>Easier Vercel deployment, documented <Link href='#'>network troubleshooting</Link></>, dev: true },
     ],
   },
   {
