bdedbfd80681d2db9ee34be56fd9b3e3a70971f3
Ross Cohen <rosscohen1990@gmail.com>
2024-01-25 22:57:14 -0500

    Update _document.tsx

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -22,9 +22,9 @@ export default function MyDocument({ emotionStyleTags }: MyDocumentProps) {
 
         {/* Favicons & PWA */}
         <link rel='shortcut icon' href='/favicon.ico' />
-        <link rel='icon' type='image/png' sizes='32x32' href='/icons/favicon-32x32.png' />
-        <link rel='icon' type='image/png' sizes='16x16' href='/icons/favicon-16x16.png' />
-        <link rel='apple-touch-icon' sizes='180x180' href='/icons/apple-touch-icon.png' />
+        <link rel='icon' type='image/png' sizes='32x32' href='#' />
+        <link rel='icon' type='image/png' sizes='16x16' href='#' />
+        <link rel='apple-touch-icon' sizes='180x180' href='#' />
         <link rel='manifest' href='/manifest.json' />
         <meta name='apple-mobile-web-app-capable' content='yes' />
         <meta name='apple-mobile-web-app-status-bar-style' content='black' />
