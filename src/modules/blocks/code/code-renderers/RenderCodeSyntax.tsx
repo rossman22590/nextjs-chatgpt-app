3309e10fe8d4d02df7d05c3eaad575f11d5fbb81
Ross Cohen <rosscohen1990@gmail.com>
2024-12-18 20:50:39 -0500

    code ui

--- src/modules/blocks/code/code-renderers/RenderCodeSyntax.tsx
+++ src/modules/blocks/code/code-renderers/RenderCodeSyntax.tsx
@@ -1,7 +1,24 @@
+// import * as React from 'react';
+
+// import { Box } from '@mui/joy';
+
+
+// export function RenderCodeSyntax(props: {
+//   highlightedSyntaxAsHtml: string | null;
+//   presenterMode?: boolean;
+// }) {
+//   return (
+//     <Box
+//       component='div'
+//       aria-label='Code block'
+//       className='code-container'
+//       dangerouslySetInnerHTML={{ __html: props.highlightedSyntaxAsHtml ?? '' }}
+//       sx={props.presenterMode ? { fontSize: '125%' } : undefined}
+//     />
+//   );
+// }
 import * as React from 'react';
-
 import { Box } from '@mui/joy';
-
 
 export function RenderCodeSyntax(props: {
   highlightedSyntaxAsHtml: string | null;
@@ -9,11 +26,44 @@ export function RenderCodeSyntax(props: {
 }) {
   return (
     <Box
-      component='div'
+      component='pre'
       aria-label='Code block'
       className='code-container'
-      dangerouslySetInnerHTML={{ __html: props.highlightedSyntaxAsHtml ?? '' }}
-      sx={props.presenterMode ? { fontSize: '125%' } : undefined}
-    />
+      sx={{
+        position: 'relative',
+        margin: 0,
+        padding: 0,
+        ...(props.presenterMode ? { fontSize: '125%' } : {}),
+        '& code': {
+          display: 'block',
+          position: 'relative',
+          padding: 0,
+        },
+        '& .line-numbers-rows': {
+          position: 'absolute',
+          pointerEvents: 'none',
+          top: 0,
+          left: -40,
+          width: 30,
+          borderRight: '1px solid var(--joy-palette-neutral-400)',
+          userSelect: 'none',
+          '& > span': {
+            display: 'block',
+            counterIncrement: 'linenumber',
+            '&:before': {
+              content: 'counter(linenumber)',
+              color: 'var(--joy-palette-neutral-400)',
+              display: 'block',
+              paddingRight: '0.8em',
+              textAlign: 'right',
+            },
+          },
+        },
+      }}
+    >
+      <code 
+        dangerouslySetInnerHTML={{ __html: props.highlightedSyntaxAsHtml ?? '' }}
+      />
+    </Box>
   );
 }
