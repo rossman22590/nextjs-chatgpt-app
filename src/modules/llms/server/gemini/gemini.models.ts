f3f5ab98e77021f8d6ef2264bdced8a39ea1d6fd
Ross Cohen <rosscohen1990@gmail.com>
2025-04-09 11:45:43 -0400

    Update gemini.models.ts

--- src/modules/llms/server/gemini/gemini.models.ts
+++ src/modules/llms/server/gemini/gemini.models.ts
@@ -433,7 +433,7 @@ export function geminiFilterModels(geminiModel: GeminiWire_API_Models_List.Model
 
 const _sortOderIdPrefix: string[] = [
   'models/gemini-exp',
-  'models/gemini-2.5-pro-preview',
+  // 'models/gemini-2.5-pro-preview',
   'models/gemini-2.5-pro-exp',
   'models/gemini-2.5-pro',
   'models/gemini-2.0-pro',
